a3833495f67cc10d57ccc7eebf6dd1274697c4f3
code-iddih <idinyamwaya@gmail.com>
2024-08-13 16:04:35 +0300

    Adding UseState

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
-import TaskList from "./TaskList";
+import TaskList from "./TaskList"
 
 import { CATEGORIES, TASKS } from "../data";
 
